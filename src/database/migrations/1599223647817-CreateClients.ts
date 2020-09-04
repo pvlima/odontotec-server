4ab8049ce7fcb849aa6cc01d13f882d73810b92d
Paulo Vitor <pv.lima02@gmail.com>
2020-09-04 10:05:39 -0300

    CreateClients migration

---
+++ src/database/migrations/1599223647817-CreateClients.ts
@@ -0,0 +1,103 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateClients1599223647817 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'clients',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'alias',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+          },
+          {
+            name: 'rg',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'cpf',
+            type: 'int',
+            isNullable: true,
+          },
+          {
+            name: 'birth',
+            type: 'date',
+            isNullable: true,
+          },
+          {
+            name: 'address',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        indices: [
+          {
+            name: 'clients_name_idx',
+            columnNames: ['name'],
+          },
+          {
+            name: 'clients_email_uidx',
+            columnNames: ['email'],
+            isUnique: true,
+          },
+          {
+            name: 'clients_phone_uidx',
+            columnNames: ['phone'],
+            isUnique: true,
+          },
+          {
+            name: 'clients_rg_uidx',
+            columnNames: ['rg'],
+            isUnique: true,
+          },
+          {
+            name: 'clients_cpf_uidx',
+            columnNames: ['cpf'],
+            isUnique: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('clients');
+  }
+}
